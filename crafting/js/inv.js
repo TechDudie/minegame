bc23dba98384c026664d5f30647386e01044d9b6
TechDudie <73961295+TechDudie@users.noreply.github.com>
2021-04-27 10:24:17 -0500

    Update inv.js

--- crafting/js/inv.js
+++ crafting/js/inv.js
@@ -1,1 +1,11 @@
-
+var storage = window.localStorage;
+var i;
+function store(id,data) {
+  storage.setItem(id, data);
+}
+function get(id) {
+  return storage.getItem(id);
+}
+for (i = 1; i < 28; i++) { 
+  //document.getElementById("inv".concat(toString(i))).appendChild(null);
+}b
