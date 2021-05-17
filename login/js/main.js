d5a734498b36867626db2d3e68a012fc56608282
TechDudie <73961295+TechDudie@users.noreply.github.com>
2021-05-17 11:07:59 -0500

    Update main.js

--- login/js/main.js
+++ login/js/main.js
@@ -1,1 +1,13 @@
-//md5("Hello World")
+document.addEventListener("keydown",  e => {
+  if (e.keyCode === 8 || e.keyCode === 13)  {
+    e.preventDefault();
+  }
+});
+function print(text) {
+  for (var i = 0; i < text.length; i++) {
+    document.getElementById("terminal").value += text[i];
+    await new Promise(r => setTimeout(r, 300));
+  }
+}
+print("Welcome to Minegame, a open source online HTML Minecraft-themed game.");
+// TEST
